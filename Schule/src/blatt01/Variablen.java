efde7e004c2f791b8115426cd56be9cf2054d8a2
DrSchischious <dschisch@willigis-online.de>
2025-08-25 09:29:11 +0200

    Einführung Variablen

---
+++ Schule/src/blatt01/Variablen.java
@@ -0,0 +1,14 @@
+package blatt01;
+
+public class Variablen {
+    public static void main(String[] args) {
+        String name = "Daniel"; //Komplexer Datentyp
+        int age = 34; //Primitiver Datentyp
+        double salary = 4677.75;
+        salary = salary + 250;
+        boolean is_male = true;
+        char gender = 'M';
+
+        System.out.println("Hallo " + name + "!");
+    }
+}
