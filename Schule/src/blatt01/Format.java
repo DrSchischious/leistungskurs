6372d496d84cabc5b9241a9adad4c94491e622b1
DrSchischious <cloud-strife@hotmail.de>
2025-09-01 09:28:12 +0200

    Formatierungstest

---
+++ Schule/src/blatt01/Format.java
@@ -0,0 +1,14 @@
+package blatt01;
+
+public class Format {
+    public static void main(String[] args) {
+        String s = "hello";
+        char c = 'H';
+        boolean b = true;
+        int d = -214;
+        double f = 41571.289121972;
+
+        System.out.printf("%f",d);
+        //System.out.println("Hello World");
+    }
+}
